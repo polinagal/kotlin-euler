2c2003ef219f623df4fce545cd4d31c3ca6e9517
Franck Rasolo <franck.rasolo@gmail.com>
2012-05-05 17:05:07 +0100

    Added average execution time and refactored a little

--- src/euler/Problem-0017.kt
+++ src/euler/Problem-0017.kt
@@ -1,43 +1,42 @@
 package euler.problem0017
 
 fun main(args : Array<String>) {
-    var result = 0
-    1..1000 forEach {
-        result += say(it).letters()
-    }
-    println(result)
+  val range = 1..1000
+
+  // average execution time of 3.9711 milliseconds over 10 iterations
+  var result = 0
+  range forEach { result += writeOut(it).countLetters() }
+
+  println("$result letters would be required to write out all the numbers from ${range.start} to ${range.end} inclusive")
 }
 
-fun say (val number : Int) : String {
-    return when (number) {
-        1 -> "one"
-        2 -> "two"
-        3 -> "three"
-        4 -> "four"
-        5 -> "five"
-        6 -> "six"
-        7 -> "seven"
-        8 -> "eight"
-        9 -> "nine"
-        10 -> "ten"
-        11 -> "eleven"
-        12 -> "twelve"
-        13 -> "thirteen"
-        14 -> "fourteen"
-        15 -> "fifteen"
-        18 -> "eighteen"
-        in arrayList(16, 17, 19) -> say(number % 10) + "teen"
-        20 -> "twenty"
-        30 -> "thirty"
-        40 -> "forty"
-        50 -> "fifty"
-        80 -> "eighty"
-        in arrayList(60, 70, 90) -> say(number / 10) + "ty"
-        in 21..99 -> say(number / 10 * 10) + "-" + say(number % 10)
-        in 100..999 -> say(number / 100) + " hundred" + if (number % 100 == 0) "" else " and " + say(number % 100)
-        1000 -> "one thousand"
-        else -> throw IllegalArgumentException()
-    }
+fun writeOut(number : Int) : String = when (number) {
+  1 -> "one"
+  2 -> "two"
+  3 -> "three"
+  4 -> "four"
+  5 -> "five"
+  6 -> "six"
+  7 -> "seven"
+  8 -> "eight"
+  9 -> "nine"
+  10 -> "ten"
+  11 -> "eleven"
+  12 -> "twelve"
+  13 -> "thirteen"
+  15 -> "fifteen"
+  18 -> "eighteen"
+  in arrayList(14, 16, 17, 19) -> writeOut(number % 10) + "teen"
+  20 -> "twenty"
+  30 -> "thirty"
+  40 -> "forty"
+  50 -> "fifty"
+  80 -> "eighty"
+  in arrayList(60, 70, 90) -> writeOut(number / 10) + "ty"
+  in 21..99 -> writeOut(number / 10 * 10) + "-" + writeOut(number % 10)
+  in 100..999 -> writeOut(number / 100) + " hundred" + if (number % 100 == 0) "" else " and " + writeOut(number % 100)
+  1000 -> "one thousand"
+  else -> throw IllegalArgumentException()
 }
 
-fun String.letters() = this.toCharList().filterNot{it == ' ' || it == '-'}.size
+inline fun String.countLetters() = toCharList().filterNot { it == ' ' || it == '-' }.size
