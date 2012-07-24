35af84948bc4be05f6a12e1dd5511ed324304b8e
Franck Rasolo <franck.rasolo@gmail.com>
2012-07-25 00:38:10 +0100

    Fixed regression in solution to problem #1

--- src/main/kotlin/euler/Problem-0001.kt
+++ src/main/kotlin/euler/Problem-0001.kt
@@ -3,11 +3,11 @@ package euler.problem0001
 import euler.multipleOf
 import euler.sum
 
-fun main(args : Array<String>) {
+fun main(args: Array<String>) {
   val limit = 1000
 
   // average execution time of 0.3447 milliseconds over 10 iterations
-  val result = (1..limit).filter { n -> n multipleOf 3 || n multipleOf 5 }.sum()
+  val result = (1..limit - 1).filter { n -> n multipleOf 3 || n multipleOf 5 }.sum()
 
   println("the sum of all the multiples of 3 or 5 below $limit is $result")
 }
--- src/main/kotlin/euler/Problem-0021.kt
+++ src/main/kotlin/euler/Problem-0021.kt
@@ -2,7 +2,7 @@ package euler.problem0021
 
 import euler.sum
 
-fun main(args : Array<String>) {
+fun main(args: Array<String>) {
   val limit = 10000
   // average execution time of 596.9915 milliseconds over 10 iterations
   val amicableNumbers = 1..limit filter { it.hasAmicablePair() }
@@ -14,4 +14,4 @@ inline fun Int.hasAmicablePair(): Boolean {
   return this != candidate && this == candidate.properDivisors().sum()
 }
 
-inline fun Int.properDivisors() = 1 upto this - 1 filter { this % it == 0 }
+inline fun Int.properDivisors() = (1..this - 1) filter { this % it == 0 }
